84deada29a132e24428022637972493dc7990fb2
David Holstius <david.holstius@gmail.com>
2021-02-17 10:31:10 -0800

    rework addTileServerLayers()

--- R/addTileServerLayers.R
+++ R/addTileServerLayers.R
@@ -1,33 +1,32 @@
+#' @export
 addTileServerLayer <- function (
-  leaflet_map,
-  layer_name,
-  group_name = layer_name,
-  tile_options = tileOptions(),
+  map_object,
+  tileset,
+  group,
+  options = pathOptions(),
   ...,
   verbose = getOption("verbose")
 ) {
   msg <- function (...) if(isTRUE(verbose)) message("[addTileServerLayer] ", ...)
-  msg("adding ", layer_name, " as \"", group_name, "\"")
+  msg("adding ", tileset, " as \"", group, "\"")
   leaflet::addTiles(
-    leaflet_map,
-    layer_name,
-    group = group_name,
-    options = tile_options,
+    map_object,
+    urlTemplate = tileset,
+    group = group,
+    options = options,
     ...)
 }
 
 #' @export
 addTileServerLayers <- function (
-  leaflet_map,
-  tileset_providers,
-  tile_options,
+  map_object,
+  tilesets,
   ...
 ) {
   reduce2(
-    tileset_providers,
-    names(tileset_providers),
+    tilesets,
+    names(tilesets),
     .f = addTileServerLayer,
-    .init = leaflet_map,
-    tile_options = tile_options,
+    .init = map_object,
     ...)
 }
