4298aa04aa4315cc4800afdfde86bb6f8030b225
David Holstius <david.holstius@gmail.com>
2021-02-17 10:29:05 -0800

    add addOverlay()

---
+++ R/addOverlay.R
@@ -0,0 +1,79 @@
+#' addOverlay
+#'
+#' @param fun leaflet function like [leaflet::addCircleMarkers()]
+#' @param map_object leaflet map
+#' @param data passed to `fun`
+#' @param group passed to `fun`
+#' @param layer_id passed to `fun`
+#' @param visible `TRUE` or `FALSE` (initial state)
+#' @param ... passed to `fun`
+#' @param verbose display messages
+#'
+#' @return
+#' @export
+addOverlay <- function (
+  fun,
+  map_object,
+  data,
+  group,
+  color = "black",
+  label = NULL,
+  layer_id = NULL,
+  visible = TRUE,
+  weight = 1.0,
+  opacity = 1.0,
+  ...,
+  verbose = TRUE
+) {
+
+  msg <- function (...) if(isTRUE(verbose)) message("[addOverlay] ", ...)
+  data <- st_transform(data, geotools::WGS84_GPS)
+
+  CMYK_COLORS <- list(
+    cyan = "#00ffff",
+    magenta = "#ff00ff",
+    yellow = "#ffff00",
+    black = "#000000",
+    purple = "#7f7fff")
+
+  if (isFALSE(is.null(names(data)))) {
+    label <- rlang::eval_tidy(label, data = data)
+  }
+
+  arg_list <- list(
+    map = map_object,
+    data = data,
+    group = group,
+    layerId = layer_id,
+    weight = weight,
+    opacity = opacity,
+    label = label,
+    ...)
+
+  if ("color" %in% names(rlang::fn_fmls(fun))) {
+    if (color %in% names(CMYK_COLORS)) {
+      color <- CMYK_COLORS[[color]]
+    }
+    arg_list <- append(arg_list, list(color = color))
+  } else {
+    if (!is.null(color)) {
+      msg("warning: color is not recognized by ", deparse(substitute(fun)))
+    }
+  }
+
+  recognized_args <-
+    arg_list[intersect(names(arg_list), names(rlang::fn_fmls(fun)))]
+
+  #msg("names(rlang::fn_fmls(fun)) is: ", str_csv(names(rlang::fn_fmls(fun))))
+  #msg("recognized_args is: ", names(recognized_args))
+
+  map_object <-
+    do.call(fun, recognized_args)
+
+  if (group %not_in% names(getOverlayGroups(map_object))) {
+    map_object <- addOverlayGroup(map_object, group, visible = visible)
+  }
+
+  return(map_object)
+
+}
